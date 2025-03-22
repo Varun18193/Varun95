99e6b4375b41a1f0e36047b3c7c9ea36f78d00ff
Varun18193 <varun.thimmaiah@gmail.com>
2025-03-22 21:03:58 +0530

    Update app.py

--- day-13/simple-python-app/app.py
+++ day-13/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! Welcome to a new unviverse!'
+    return 'Hello, world! Welcome to a new unviverse of just living life the way it is!'
 
 if __name__ == '__main__':
     app.run()
