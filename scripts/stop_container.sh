b688a0042fc25e1246a1372b56e646b1e3790998
Varun18193 <varun.thimmaiah@gmail.com>
2025-03-22 18:28:03 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Get the IDs of running containers
-docker ps -a
 # Stop the running container (if any)
 containerid= docker ps | awk -F " " '{print $1}'
 #stopping and removing container
