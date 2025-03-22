23756c00c12533a92c980a84cb53f163f6f846df
Varun18193 <varun.thimmaiah@gmail.com>
2025-03-22 20:40:16 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-set -e
 
-# Stop the running container (if any)
-containerid= docker ps | awk -f " " '{print $1}'
-docker rm -f $containerid
+echo "Stopping existing container..."
+
+# Get the container ID running on port 5000
+CONTAINER_ID=$(docker ps -q --filter "publish=5000")
+
+# Stop the container if it's running
+if [ -n "$CONTAINER_ID" ]; then
+  echo "Stopping container: $CONTAINER_ID"
+  docker stop "$CONTAINER_ID"
+  docker rm "$CONTAINER_ID"
+else
+  echo "No running container found on port 5000."
+fi
+
 
